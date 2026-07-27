7b10a0e3aa5f03dd46ace04d89d78290315ec0c8
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-07-27 18:36:09 +0500

    Buy and Sell Stocks

--- buysellstocks.cpp
+++ buysellstocks.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
+void buy (int *price , int n) {
+    int bestBuy [100000] ;
+    bestBuy [0] = INT_MAX;
+    int maxProfit = 0;
+    for (int i =1 ; i < n ; i++){
+        bestBuy[i] = min(bestBuy[i-1], price[i-1]);
+        int profit = price[i] - bestBuy[i];
+        maxProfit = max(maxProfit,profit);
+    }
+    cout << maxProfit;
+}
+
 int main(){
-    
+    int prices[6] = {7,1,5,3,6,4};
+    int n = sizeof(prices) / sizeof(int) ;
+    buy(prices, n);
     return 0;
 }
