7e84228b72122bcc8e14df37625be86da65bf46e
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-07-28 17:03:24 +0500

    Problem Solved

--- trappingrainwater.cpp
+++ trappingrainwater.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 using namespace std;
-#include <climits>
 
-void trap (int *heights, int n ){
+void trap(int *heights, int n) {
     int leftMax[20000];
-    int maximum = 0;
-    leftMax[0] = INT_MIN;
-    for (int i = 1; i < n; i++)
-    {
-        leftMax[i] = max(leftMax[i-1],heights[i-1]);
-    }
-
     int rightMax[20000];
-    rightMax[n-1];
-    for (int i = n ; i < 1; i--){
-        rightMax[i] = max(rightMax[i-1],heights[i-1]);
-    }
-    
-} 
 
-int main(){
-    int height[8] = {4,2,0,6,0,3,2,5};
+    // Left max
+    leftMax[0] = heights[0];
+    for (int i = 1; i < n; i++) {
+        leftMax[i] = max(leftMax[i-1], heights[i-1]);
+    }
+
+    // Right max
+    rightMax[n - 1] = heights[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+        rightMax[i] = max(rightMax[i + 1], heights[i+1]); 
+    }
+
+    // Water calculation
+    int waterTrapped = 0;
+    for (int i = 0; i < n; i++) {
+        int currWater = min(leftMax[i], rightMax[i]) - heights[i];
+        if (currWater > 0) {
+            waterTrapped += currWater;
+        }
+    }
+
+    cout << waterTrapped;
+}
+
+int main() {
+    int height[8] = {4,2,0,6,3,2,5};
     int n = sizeof(height) / sizeof(int);
-    trap(height,n);
+    trap(height, n);
     return 0;
 }
