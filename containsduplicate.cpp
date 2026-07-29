e52dce1d73059b1d6e7b244d89ef9c8f97366bd2
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-07-29 14:22:49 +0500

    Contains Duplicate

---
+++ containsduplicate.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool dupl(int *nums , int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for(int k = i+1; k < n; k++){
+            if (nums[i] == nums[k])
+            {
+                return true;
+            }
+            
+        }
+    }
+
+    return false;
+    
+}
+
+int main(){
+    int nums[] = {1,2,3,4};
+    int n = sizeof(nums) / sizeof(int);
+    dupl(nums,n);
+    return 0;
+}
