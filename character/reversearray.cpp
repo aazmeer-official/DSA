38407b439ec712cf6286f259209d53042d97b208
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-08-10 14:35:00 +0500

    Reverse an Array

--- character/reversearray.cpp
+++ character/reversearray.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
+#include <cstring>
+
+void twoPointer(char arr[], int n)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
 
 int main()
 {
     char arr[5] = "code";
-
+    int n = strlen(arr);
+    twoPointer(arr, n);
+    cout << "Reversed Array is : " << arr << endl;
     return 0;
 }
