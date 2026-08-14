3f71a47a05a351fb54b376d067982f7e854a129f
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-08-14 06:54:59 +0500

    Strings Started

---
+++ strings/code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str1 = "Hello, ";
+    cout << str1 << endl;
+    str1 = "yellow";
+    cout << str1 << endl;
+    return 0;
+}
