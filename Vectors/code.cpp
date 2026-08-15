65b15be6a76f08d37493233b8e919fc8693e0667
Muhammad Aazmeer <aazmeer.official@gmail.com>
2026-08-15 23:37:14 +0500

    Vectors

--- Vectors/code.cpp
+++ Vectors/code.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
+    int size;
+    cin >> size;
+    int *arr = new int[size];
+
+    int x = 1;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = x;
+        x++;
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
